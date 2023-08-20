a9291f865f850745cc7334c148d29afbfa75e278
DanielPascod <daniel.pascoalato@uol.com.br>
2023-08-20 18:56:01 -0300

    Add files via upload

---
+++ src/main/java/daniel/CSVApplication.java
@@ -0,0 +1,12 @@
+package daniel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CSVApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CSVApplication.class, args);
+    }
+}
